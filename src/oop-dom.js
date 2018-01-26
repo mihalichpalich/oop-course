9a8f5835a7f334f10f2a5d6b8832272200bf85e2
mihalichpalich <pavlovtfspb@gmail.com>
2018-01-26 17:01:11 +0300

    изучил применение цепочки методов к объекту как в jquery

--- src/oop-dom.js
+++ src/oop-dom.js
@@ -5,13 +5,14 @@ class Elem {
 
   html(text) {
     this.elem.innerHTML = text;
+    return this;
   }
 
   attr(name, value) {
     this.elem.setAttribute(name, value);
+    return this;
   }
 }
 
 var elem = new Elem("#test");
-elem.html("!");
-elem.attr('title', 'привет');
+elem.html("!").attr('title', 'привет').attr('class', 'www');
